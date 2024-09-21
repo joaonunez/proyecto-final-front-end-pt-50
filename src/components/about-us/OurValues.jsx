ae94d39ba152c7a056b5778b736a02290bd90e4e
joaonunez <joaovaldiglesias@gmail.com>
2024-09-21 19:04:54 -0300

    avance in aplication

---
+++ src/components/about-us/OurValues.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function OurValues() {
+  return (
+    <section className="about-section">
+      <h2>Nuestros Valores</h2>
+      <ul>
+        <li>Integridad y Transparencia</li>
+        <li>Innovación y Creatividad</li>
+        <li>Respeto y Diversidad</li>
+      </ul>
+    </section>
+  );
+}
--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,25 +1,24 @@
-import image_camper from "../../assets/images/banner/banner_camper.jpg"
-import image_camping from "../../assets/images/banner/banner_camping.jpg"
-import image_glamping from "../../assets/images/banner/banner_glamping.jpg"
+import React from "react";
+import image_camper from "../../assets/images/banner/banner_camper.jpg";
+import image_camping from "../../assets/images/banner/banner_camping.jpg";
+import image_glamping from "../../assets/images/banner/banner_glamping.jpg";
 
 
 export function Banner() {
     return (
-        <>
+        <div className="col-12 banner-section">
+            <h2 className="banner-title">¡Animate a tener unas buenas vacaciones!</h2>
             <div className="col-12 container-banner">
                 <div className="col-4 image-container">
-                    <img src={image_camper} alt="" className="image-banner col-12" />
+                    <img src={image_camper} alt="Camper" className="image-banner col-12" />
                 </div>
                 <div className="col-4 image-container">
-                    <img src={image_camping} alt="" className="image-banner col-12" />
-
+                    <img src={image_camping} alt="Camping" className="image-banner col-12" />
                 </div>
                 <div className="col-4 image-container">
-                    <img src={image_glamping} alt="" className="image-banner col-12" />
+                    <img src={image_glamping} alt="Glamping" className="image-banner col-12" />
                 </div>
-
-
             </div>
-        </>
+        </div>
     );
 }
