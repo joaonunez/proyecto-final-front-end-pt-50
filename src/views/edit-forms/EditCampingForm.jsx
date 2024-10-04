e5cdd62d22f956be2e9543b2a2a5045043b0fd34
joaonunez <joaovaldiglesias@gmail.com>
2024-10-04 01:27:49 -0300

    paciente estable

--- src/views/edit-forms/EditCampingForm.jsx
+++ src/views/edit-forms/EditCampingForm.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import FormEditCamping from '../../components/formularios/FormEditCamping'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import FormEditCamping from '../../components/formularios/FormEditCamping';
+import { Context } from '../../store/context';
+import { useContext } from 'react';
 
 export function EditCampingForm() {
-  const { campingId } = useParams()
+  const { campingId } = useParams();
+  const { store } = useContext(Context);
+
+  // Obtener el providerId directamente del store
+  const providerId = store.user?.id;
+
   return (
     <>
-      < FormEditCamping id={campingId} />
+      <FormEditCamping campingId={campingId} providerId={providerId} />
     </>
-  )
+  );
 }
+
+export default EditCampingForm;
